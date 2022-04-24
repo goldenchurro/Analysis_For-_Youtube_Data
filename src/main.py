5d2ce7164f0cfd2a89bfbeb07550ccd30e584510
Brady Blundell <bradyzemailbox@gmail.com>
2022-04-24 10:53:29 -0700

    Added a second data set

--- src/main.py
+++ src/main.py
@@ -15,12 +15,31 @@ import pandas as pd
 # Compare previous two
 
 def main():
+    ############################################
+    # Create PySpark session
+    ############################################
+
     pySparkSes = SparkSession.builder.getOrCreate()
+
+    ############################################
+    # Read in text files
+    ############################################
 
     yt0518_0 = pySparkSes.read.csv("src/0518/0.txt", sep = "\t")
     yt0518_1 = pySparkSes.read.csv("src/0518/1.txt", sep = "\t")
     yt0518_2 = pySparkSes.read.csv("src/0518/2.txt", sep = "\t")
     yt0518_3 = pySparkSes.read.csv("src/0518/3.txt", sep = "\t")
+
+    ####  Have one set or the other commented out  ####
+
+    #yt0518_0 = pySparkSes.read.csv("src/080518/0.txt", sep = "\t")
+    #yt0518_1 = pySparkSes.read.csv("src/080518/1.txt", sep = "\t")
+    #yt0518_2 = pySparkSes.read.csv("src/080518/2.txt", sep = "\t")
+    #yt0518_3 = pySparkSes.read.csv("src/080518/3.txt", sep = "\t")
+
+    ############################################
+    # Rename Columns
+    ############################################
 
     yt0518_0 = yt0518_0.withColumnRenamed('_c0', "ID").withColumnRenamed('_c1', "Uploader")
     yt0518_0 = yt0518_0.withColumnRenamed('_c2', "Age").withColumnRenamed('_c3', "Category")
@@ -49,6 +68,10 @@ def main():
     yt0518_3 = yt0518_3.withColumnRenamed('_c6', "Rate").withColumnRenamed('_c7', "Ratings")
     yt0518_3 = yt0518_3.withColumnRenamed('_c8', "Comments").withColumnRenamed('_c9', "Related")
     yt0518_3 = yt0518_3.drop(*[str(x) for x in yt0518_3.columns[9:-1]])
+
+    ############################################
+    # Union dataframes together
+    ############################################
 
     ytUnion = yt0518_0.union(yt0518_1).union(yt0518_2).union(yt0518_3)
 
